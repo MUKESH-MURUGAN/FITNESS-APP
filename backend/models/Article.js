9f11458adb4965633428dbf42bbf7d0315def6aa
parthasarathis26 <parthasarathis.22cse@kongu.edu>
2025-04-29 21:22:02 +0530

    Added Artcle.js and Comment.js

---
+++ backend/models/Article.js
@@ -0,0 +1,17 @@
+const articleSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    content: { type: String, required: true }, // Rich text
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    type: { type: String, enum: ['Fitness', 'Wellness', 'Diet', 'Workout'], required: true },
+    visibility: { type: String, enum: ['public', 'private', 'members'], default: 'public' },
+    mediaAttachments: [String],
+  
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    ratings: [{
+      user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      score: { type: Number, min: 1, max: 5 }
+    }],
+  }, { timestamps: true });
+  
+  module.exports = mongoose.model('Article', articleSchema);
+  
