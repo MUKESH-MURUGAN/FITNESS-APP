217e09d4279ed71a640610049fd02d75e329c14a
pavi6804 <125538527+pavi6804@users.noreply.github.com>
2025-05-02 10:58:06 +0530

    database-cnnection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const app = require('./src/app');
 const PORT = 3000;
 
+app.get('/',(req,res)=>{
+  res.send("hi this a fitness and wellness content management system");
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
